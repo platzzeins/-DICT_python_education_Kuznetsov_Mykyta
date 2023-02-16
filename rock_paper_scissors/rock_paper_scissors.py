c68e4b411c472192c93b1ab7e4e0d26e48fd23dc
DIT Happy <m.h.kuznetsov@student.khai.edu>
2023-02-16 14:32:06 +0200

    2-nd Stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,5 +1,7 @@
-combinations_win = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
-combinations_fall = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+import random
+
+combinations = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
+options = ["rock", "paper", "scissors"]
 
 
 class game():
@@ -7,14 +9,25 @@ class game():
         self.userOption = userChoice
         self.botOption = ""
 
-    def win_bot(self):
-        self.botOption = combinations_fall[self.userOption]
-        print(self.userOption)
-        print(f"Sorry, but the computer chose {self.botOption}")
+    def actual_game(self):
+        self.botOption = random.choice(options)
+
+        if self.userOption.__eq__(self.botOption):
+            print(f"Bot chose {self.botOption}")
+            print("Draw")
+        for i, j in combinations.items():
+            if self.userOption == i and self.botOption == j:
+                print(f"Bot chose {self.botOption}")
+                print("First player won!")
+                break
+            elif self.botOption == i and self.userOption == j:
+                print(f"Bot chose {self.botOption}")
+                print("Second player won!")
+                break
 
 
 if __name__ == "__main__":
     while True:
         userChoice = input("Input your option ")
         new = game(userChoice)
-        new.win_bot()
+        new.actual_game()
